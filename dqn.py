c3b9a2f182d00041b2666c63c26acb270b6c4164
송형선(HyungSun Song) <hssongng@gmail.com>
2017-11-08 20:43:59 +0900

    adjust hyper parameters

--- dqn.py
+++ dqn.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
-import gym
-import math
-import random
-import matplotlib
-import matplotlib.pyplot as plt
 from collections import namedtuple
-from collections import deque
-from itertools import count
-from copy import deepcopy
-from PIL import Image
 import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.autograd import Variable
-import torchvision.transforms as T
-
 import cv2
 import sys
 sys.path.append("game/")
@@ -25,23 +13,18 @@ import wrapped_flappy_bird as game
 import random
 
 ACTIONS = 2 # number of valid actions
-GAMMA = 0.99 # decay rate of past observations
+GAMMA = 0.99 # discount factor
+LEARNING_RATE = 0.00025
 REPLAY_MEMORY = 1000000 # number of previous transitions to remember
-MINIMUM_REPLAY_SIZE = 5000
+MINIMUM_REPLAY_SIZE = 50000
 BATCH_SIZE = 32 # size of minibatch
-FRAME_PER_ACTION = 1
-EPSILON_FIRST = 1.0 # Exploration rate
-EPSILON_FINAL = 0.001
-EPSILON_DECAY = 200
+INITIAL_EPSILON = 1.0 # Exploration rate
+FINAL_EPSILON = 0.1
+EPSILON_CONVERGE = 1000000
 OBSERVED = 50000
-M = 100000 # episode count
+MOMENTUM = 0.95
 C = 10000 # Q reset interval
-# set up matplotlib
-is_ipython = 'inline' in matplotlib.get_backend()
-if is_ipython:
-    from IPython import display
-
-plt.ion()
+NO_OP_MAX = 30
 
 # if gpu is to be used
 use_cuda = torch.cuda.is_available()
@@ -52,10 +35,6 @@ Tensor = FloatTensor
 
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
-resize = T.Compose([T.ToPILImage(),
-                    T.Scale(40, interpolation=Image.CUBIC),
-                    T.ToTensor()])
-
 
 class ReplayMemory(object):
 
@@ -65,7 +44,6 @@ class ReplayMemory(object):
         self.position = 0
 
     def push(self, *args):
-        """Saves a transition."""
         if len(self.memory) < self.capacity:
             self.memory.append(None)
         self.memory[self.position] = Transition(*args)
@@ -73,10 +51,6 @@ class ReplayMemory(object):
 
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
-
-    def downsize(self):
-        if len(self.memory) > REPLAY_MEMORY:
-            self.memory = self.memory[OBSERVED:]
 
     def __len__(self):
         return len(self.memory)
@@ -106,6 +80,7 @@ class DQN(nn.Module):
         return x
 
 
+
 Q = DQN()
 target_Q = DQN()
 
@@ -113,7 +88,7 @@ if use_cuda:
     Q.cuda()
     target_Q.cuda()
 
-optimizer = optim.RMSprop(Q.parameters())
+optimizer = optim.RMSprop(Q.parameters(), lr=LEARNING_RATE)
 memory = ReplayMemory(REPLAY_MEMORY)
 
 
@@ -122,52 +97,52 @@ steps_done = 0
 def select_action(state):
     global steps_done
     steps_done += 1
-    action = np.zeros([ACTIONS])
-    eps_threshold = EPSILON_FINAL + (EPSILON_FIRST - EPSILON_FINAL) * math.exp(-1. * steps_done / EPSILON_DECAY)
+    action = torch.zeros(ACTIONS)
 
+    if steps_done < EPSILON_CONVERGE:
+        eps_threshold = INITIAL_EPSILON - ((INITIAL_EPSILON - FINAL_EPSILON) / EPSILON_CONVERGE * steps_done)
+    else :
+        eps_threshold = FINAL_EPSILON
     if random.random() <= eps_threshold:
-        #print("----------Random Action----------", steps_done)
+
         action_index = random.randrange(ACTIONS)
         action[action_index] = 1
+        #print("----------Random Action----------", steps_done, action_index)
     else:
-        readout = Q(Variable(state, volatile=True).type(FloatTensor)).data.max(0)[0]
-        readout = readout.unsqueeze(0).type(Tensor)
-        action_index = np.argmax(readout)
+        readout = Q(Variable(state, volatile=True).type(FloatTensor))
+        action_index = readout.max(1)[1].data[0]
         action[action_index] = 1
-    action = torch.from_numpy(action)
+        print(readout.data, action_index, steps_done)
     return action
 
 
 def optimize_model():
-    global steps_done
-
     if steps_done % C == 0:
         target_Q.load_state_dict(Q.state_dict())
 
     if len(memory) < MINIMUM_REPLAY_SIZE:
         return
 
-    transitions = memory.sample(BATCH_SIZE) # Batch_size만큼 샘플 추출
+    transitions = memory.sample(BATCH_SIZE)
 
     batch = Transition(*zip(*transitions))
 
     # Compute a mask of non-final states and concatenate the batch elements
-    non_final_mask = ByteTensor(tuple(map(lambda s: s is not None,
-                                          batch.next_state)))
+    non_final_mask = ByteTensor(tuple(map(lambda s: s is not None, batch.next_state)))
 
     non_final_next_states = Variable(torch.cat([s for s in batch.next_state
-                                                if s is not None]),
+                                                if s is not None]).type(Tensor),
                                      volatile=True)
 
-    state_batch = Variable(torch.cat(batch.state))  # 32개의 샘플 중 state만 모아놓음
-    reward_batch = Variable(torch.cat(batch.reward))
+    state_batch = Variable(torch.cat(batch.state).type(Tensor))
+    reward_batch = Variable(torch.cat(batch.reward).type(Tensor))
     Q_Values = Q(state_batch).max(1)[0]
 
     target_Q_values = Variable(torch.zeros(BATCH_SIZE).type(Tensor))
     target_Q_values[non_final_mask] = target_Q(non_final_next_states).max(1)[0]
     target_Q_values.volatile = False
 
-    y_values = reward_batch + (target_Q_values * GAMMA)
+    y_values = reward_batch + (GAMMA * target_Q_values)
     loss = F.mse_loss(y_values, Q_Values)
     # Optimize the model
     optimizer.zero_grad()
@@ -177,43 +152,44 @@ def optimize_model():
     optimizer.step()
 
 
-
-
 if __name__ == '__main__':
 
-    # for i in range(0, M) :
+    target_Q.load_state_dict(Q.state_dict())
+
+    # M = 1
+    # for a in range(0,M):
     while True:
         game_state = game.GameState()
         do_nothing = torch.zeros(ACTIONS)
         do_nothing[0] = 1
         x_0, r_0, terminal = game_state.frame_step(do_nothing)
         x_0 = cv2.cvtColor(cv2.resize(x_0, (80, 80)), cv2.COLOR_BGR2GRAY)
-        ret, x_0 = cv2.threshold(x_0, 1, 255, cv2.THRESH_BINARY)
+        _, x_0 = cv2.threshold(x_0, 1, 255, cv2.THRESH_BINARY)
 
-        x_0 = torch.from_numpy(x_0).type(FloatTensor).unsqueeze(0)
-        s_0 = torch.cat((x_0, x_0, x_0, x_0), dim=0)  # 맨 처음 초기 상태 s를 구함
+        x_0 = torch.from_numpy(x_0).unsqueeze(0)
+        s_0 = torch.cat((x_0, x_0, x_0, x_0), dim=0)
         state = s_0.unsqueeze(0)
         a_0 = select_action(state)
         action = a_0
 
         while True:
             if terminal:
-                # Optimize the model
-                optimize_model()
-                memory.downsize()
                 break
             action = select_action(state)
-            x_t_colored, reward, terminal = game_state.frame_step(action)  # 초기 상태에서 action을 선택한 대로 진행
+            x_t_colored, reward, terminal = game_state.frame_step(action)
             #print(reward)
             x_t = cv2.cvtColor(cv2.resize(x_t_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
-            ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
-            x_t = torch.from_numpy(x_t).type(FloatTensor).unsqueeze(0)
+            _, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
+            x_t = torch.from_numpy(x_t).unsqueeze(0)
             temp_state = state.squeeze(0)
             next_state = torch.cat((x_t, temp_state[:3, ...]), dim=0).unsqueeze(0)
-            reward = FloatTensor([reward])
+            reward = torch.FloatTensor([reward])
             memory.push(state, action, next_state, reward)
             state = next_state
+            # Optimize the model
+            optimize_model()
 
 print('Complete')
+
 
 
